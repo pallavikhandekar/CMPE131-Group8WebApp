792356d63b58e0d634cbc74c152a8ae31b0359f5
sakshikalra <sakshi38895@gmail.com>
2013-05-11 20:23:51 -0700

    Added code to load and upload csv to mysql database

---
+++ SANraMonDataLoader/src/LoadCSVToDB.java
@@ -0,0 +1,87 @@
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import au.com.bytecode.opencsv.CSVReader;
+
+//import java.util.Date;
+
+public class LoadCSVToDB {
+  private Connection connect = null;
+  private Statement statement = null;
+  private PreparedStatement preparedStatement = null;
+  private ResultSet resultSet = null;
+
+  public void readDataBase() throws Exception {
+    try {
+      // This will load the MySQL driver, each DB has its own driver
+      Class.forName("com.mysql.jdbc.Driver");
+      
+      // Setup the connection with the DB
+      // This assumes that we have database of name sanramon and there is a user
+      // sanramon having the password sanramon who has all the privileges
+      // to insert the data
+      connect = DriverManager
+          .getConnection("jdbc:mysql://localhost/sanramon?"
+              + "user=sanramon&password=sanramon");
+
+      // Statements allow to issue SQL queries to the database
+      statement = connect.createStatement();
+      
+     
+      String csvFilename = "/my/Sample_Org_Data_1.csv";
+      CSVReader csvReader = new CSVReader(new FileReader(csvFilename));
+      String[] row = null;
+      while((row = csvReader.readNext()) != null) {
+      
+   
+          // PreparedStatements can use variables and are more efficient
+          // It assumes a table of name sanramon_users having following columns:
+          // (Company, Department, UserID, User,IPAddress, LUN, IOChannel);
+          preparedStatement = connect.prepareStatement("insert into sanramon.sanramon_users values (?, ?, ?, ?, ? , ?, ?)");
+      
+          preparedStatement.setString(1, row[0]);
+          preparedStatement.setString(2, row[1]);
+          preparedStatement.setString(3, row[2]);
+          preparedStatement.setString(4, row[3]);
+          preparedStatement.setString(5, row[4]);
+          preparedStatement.setString(6, row[5]);
+          preparedStatement.setString(7, row[6]);
+          preparedStatement.executeUpdate();
+      }
+       
+      csvReader.close();
+   
+     
+      
+    } catch (Exception e) {
+      throw e;
+    } finally {
+      close();
+    }
+
+  }
+
+  // You need to close the resultSet
+  private void close() {
+    try {
+      if (resultSet != null) {
+        resultSet.close();
+      }
+
+      if (statement != null) {
+        statement.close();
+      }
+
+      if (connect != null) {
+        connect.close();
+      }
+    } catch (Exception e) {
+
+    }
+  }
+
+} 
