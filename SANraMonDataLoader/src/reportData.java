f74a40a587afc076d9b2880fc41dab69065b0355
Raj Ruprai <rajwinder.ruprai@sjsu.edu>
2013-05-12 19:43:42 -0700

    Modified reportData so that it is easier to read

--- SANraMonDataLoader/src/reportData.java
+++ SANraMonDataLoader/src/reportData.java
@@ -11,25 +11,32 @@ public class reportData extends MonitoringData {
 	 * Author: Rajwinder Ruprai
 	 * @throws Exception 
 	 */
-	public static void main(String[] args) throws Exception {
-		int i=0;
+	public void accessData() throws Exception {
 		ArrayList<String> tStamp = new ArrayList<String>();
 		ArrayList<String> iops = new ArrayList<String>();
 		try {
+			int i=0;
 			Class.forName("com.mysql.jdbc.Driver");
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost/sanramon?" + "user=sanramon&password=sanramon");
 			Statement st = con.createStatement();
-			ResultSet rs = st.executeQuery("SELECT MON_TIMESTAMP, IOPS FROM sanramon.monitor_data;");
+			//query only timestamp (x-axis) and iops (y-axis) so we can do a graph based on those two fields
+			ResultSet rs = st.executeQuery("SELECT MON_TIMESTAMP, IOPS FROM sanramon.monitor_data;"); 
+			
+			/*
+			 * If anyone want to get a random time stamp and iops information,
+			 * they would have to call tStamp.get(i) and iops.get(i) where i is the index 
+			 * the "i" has to be the same number
+			 * essentially two arraylists were created so index 2 in tStamp goes with index 2 in iops, for example				 * 
+			 */
 			
 			while (rs.next()) {
-				tStamp.add(rs.getString("MON_TIMESTAMP"));
-				iops.add(rs.getString("IOPS"));
-				System.out.print(tStamp.get(i)+"\t"+iops.get(i)+"\n");
+				tStamp.add(rs.getString("MON_TIMESTAMP"));//add the appropriate time stamp
+				iops.add(rs.getString("IOPS")); //also add the corresponding iops for that time stamp				
 				i++;
 			}//end while
 		}//end try
 		catch(Exception ex) {
 			throw ex;
 		}//end catch
-	}//end main
+	}//end accessData
 }//end reportData
