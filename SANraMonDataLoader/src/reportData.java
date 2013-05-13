2f688a72dd52526c231c8b23b25cf6c0448dfe27
Raj Ruprai <rajwinder.ruprai@sjsu.edu>
2013-05-12 18:21:43 -0700

    This is my second attempt to upload reportData java class that is able to pull data from db

--- SANraMonDataLoader/src/reportData.java
+++ SANraMonDataLoader/src/reportData.java
@@ -12,25 +12,24 @@ public class reportData extends MonitoringData {
 	 * @throws Exception 
 	 */
 	public static void main(String[] args) throws Exception {
-		//String url = null;
-		//String usname = null;
-		//String pwrd = null;
-		ArrayList<String> list = new ArrayList();
+		int i=0;
+		ArrayList<String> tStamp = new ArrayList<String>();
+		ArrayList<String> iops = new ArrayList<String>();
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost/sanramon?" + "user=sanramon&password=sanramon");
 			Statement st = con.createStatement();
-			System.out.print("It is connecting");
-			ResultSet rs = st.executeQuery("SELECT * FROM sanramon");
+			ResultSet rs = st.executeQuery("SELECT MON_TIMESTAMP, IOPS FROM sanramon.monitor_data;");
 			
 			while (rs.next()) {
-				list.add(rs.getString("userID"));
-			}
-		}catch(Exception ex) {
+				tStamp.add(rs.getString("MON_TIMESTAMP"));
+				iops.add(rs.getString("IOPS"));
+				System.out.print(tStamp.get(i)+"\t"+iops.get(i)+"\n");
+				i++;
+			}//end while
+		}//end try
+		catch(Exception ex) {
 			throw ex;
-			
-		}
-
-	}
-
-}
+		}//end catch
+	}//end main
+}//end reportData
